859c8d962908def31d40279c803894b937c5ff5c
smaksimenkod <smaksimenko@lohika.com>
2013-10-14 14:08:26 +0300

    added new cherity foundation

--- tim/memorystore.js
+++ tim/memorystore.js
@@ -222,6 +222,13 @@ window.store = {
             link: "http://spasibo.od.ua/sos-%D0%BC%D0%BB%D0%B0%D0%B4%D0%B5%D0%BD%D0%B5%D1%86-%D1%82%D0%B8%D0%BC%D0%BE%D1%84%D0%B5%D0%B9-%D1%81%D0%B0%D0%BB%D1%82%D0%B0%D0%BD%D0%BE%D0%B2%D1%81%D0%BA%D0%B8%D0%B9-%D0%B2-%D0%B1%D0%BE%D1%80/",
             alias: "Перейти на сайт"
         };
+        this.foundations[3] = {
+            id: 3,
+            personaId: 1,
+            name: "Благотворительная организация 'Благовещение'",
+            link: "http://blagovest.od.ua/actual/207-saltanovskiy-timofey.html",
+            alias: "Перейти на сайт"
+        };
         this.advert[1] = {
             id: 1,
             actionDate: new Date(2013, 9, 6).getTime(),
